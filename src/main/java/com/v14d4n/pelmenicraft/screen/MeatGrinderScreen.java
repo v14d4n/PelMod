ff79e294fcbbce71e16ef5e8ca613d6590ce0117
Vladislav "v14d4n" Nefedov <vlad_nefedov_2002@mail.ru>
2022-01-13 14:44:26 +0400

    fix meat grinder screen

--- src/main/java/com/v14d4n/pelmenicraft/screen/MeatGrinderScreen.java
+++ src/main/java/com/v14d4n/pelmenicraft/screen/MeatGrinderScreen.java
@@ -5,13 +5,13 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.v14d4n.pelmenicraft.PelmeniCraft;
 import com.v14d4n.pelmenicraft.container.MeatGrinderContainer;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 public class MeatGrinderScreen extends AbstractContainerScreen<MeatGrinderContainer> {
-    private final ResourceLocation GUI = new ResourceLocation(PelmeniCraft.MOD_ID,
-            "textures/gui/meat_grinder_gui.png");
+    private static final ResourceLocation GUI = new ResourceLocation(PelmeniCraft.MOD_ID, "textures/gui/meat_grinder_gui.png");
 
     public MeatGrinderScreen(MeatGrinderContainer screenContainer, Inventory inv, Component titleIn) {
         super(screenContainer, inv, titleIn);
@@ -21,15 +21,17 @@ public class MeatGrinderScreen extends AbstractContainerScreen<MeatGrinderContai
     public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
         this.renderBackground(pPoseStack);
         super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
-        this.renderLabels(pPoseStack, pMouseX, pMouseY);
+        this.renderTooltip(pPoseStack, pMouseX, pMouseY);
     }
 
     @Override
     protected void renderBg(PoseStack pPoseStack, float pPartialTick, int pX, int pY) {
-        RenderSystem.setShaderFogColor(1f, 1f, 1f, 1f); // FIXME: возможны не те цвета (clearcolor)
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, GUI);
+        int i = this.leftPos;
+        int j = this.topPos;
         this.minecraft.getTextureManager().bindForSetup(GUI);
-        int i = this.getGuiLeft();
-        int j = this.getGuiTop();
-        this.blit(pPoseStack, i, j, 0, 0, this.getXSize(), this.getYSize());
+        this.blit(pPoseStack, i, j, 0, 0, this.imageWidth, this.imageHeight);
     }
 }
